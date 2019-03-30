36bf0f6225345e5249e692c6a8f5bb666cb618cf
PaDuckk <kang10019@naver.com>
2019-03-30 11:07:25 +0900

    nav test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Menu from "./components/Menu";
+import { Container, Col, Row } from "react-bootstrap";
 import { MyMNA, MNA, Party, Bill } from "./pages";
-import "bootstrap/dist/css/bootstrap.css";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Menu />
+
         <Switch>
-          <Route exact path="/" />
-          <Route path="/mymna" component={MyMNA} />
-          <Route path="/mna" component={MNA} />
-          <Route path="/party" component={Party} />
-          <Route path="/bill" component={Bill} />
+          <Container>
+            <Route exact path="/" />
+            <Route path="/mymna" component={MyMNA} />
+            <Route path="/mna" component={MNA} />
+            <Route path="/party" component={Party} />
+            <Route path="/bill" component={Bill} />
+          </Container>
         </Switch>
       </div>
     );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,27 +1,47 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import { Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 const Menu = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <NavLink to="/">홈</NavLink>
-        </li>
-        <li>
-          <NavLink to="/mymna">내 의원</NavLink>
-        </li>
-        <li>
-          <NavLink to="/mna">의원</NavLink>
-        </li>
-        <li>
-          <NavLink to="/party">정당</NavLink>
-        </li>
-        <li>
-          <NavLink to="/bill">법안</NavLink>
-        </li>
-      </ul>
-    </div>
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <NavLink to="/">
+        <Navbar.Brand>홈</Navbar.Brand>
+      </NavLink>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="reponsive-navbar-nav">
+        <Nav className="mr-auto">
+          <NavLink to="/mymna">
+            {({ isActive, onClick, href }) => (
+              <Nav.Item>
+                <Nav.Link>내 의원</Nav.Link>
+              </Nav.Item>
+            )}
+          </NavLink>
+          <Nav.Item>
+            <LinkContainer to="/mna">
+              <Nav.Link>의원</Nav.Link>
+            </LinkContainer>
+          </Nav.Item>
+          <NavLink to="/party">
+            <Nav.Link>
+              <Nav.Item>정당</Nav.Item>
+            </Nav.Link>
+          </NavLink>
+          <NavLink to="/bill">
+            <Nav.Link>
+              <Nav.Item>법안</Nav.Item>
+            </Nav.Link>
+          </NavLink>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
